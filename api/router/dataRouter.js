31f8a6c648d22e156095e10e032e7680d0301567
owalid <othmane.el.ayadi@gmail.com>
2019-11-16 18:43:37 +0100

    new functions for report controller

--- api/router/dataRouter.js
+++ api/router/dataRouter.js
@@ -3,6 +3,13 @@ import reportController from '../controllers/reportController';
 
 export const dataRouter = Router();
 
-dataRouter.post('/', async (req, res) => reportController.getContry(req, res));
-dataRouter.post('/getWithYear', async (req, res) => reportController.getDataWithYear(req, res));
+dataRouter.post('/', async (req, res) => reportController.getAllData(req, res));
+dataRouter.post('/getYearDataWithId', async (req, res) => reportController.getDataWithIdYear(req, res));
+dataRouter.post('/getYearDataWithCountry', async (req, res) => reportController.getDataYearWithCountry(req, res));
+dataRouter.post('/getDataWithYear', async (req, res) => reportController.getDataWithYear(req, res));
+dataRouter.post('/getDataWithGas', async (req, res) => reportController.getDataWithGas(req, res));
+dataRouter.post('/getDataWithSector', async (req, res) => reportController.getDataWithSector(req, res));
+dataRouter.post('/getDataWithUnit', async (req, res) => reportController.getDataWithUnit(req, res));
+dataRouter.post('/getDataFilter', async (req, res) => reportController.getDataFilter(req, res));
+dataRouter.post('/getPrevision', async (req, res) => reportController.getPrevision(req, res));
 
