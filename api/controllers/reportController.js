8a9339f3c726399eea2a2ef54a73e0c189d14f96
owalid <othmane.el.ayadi@gmail.com>
2019-11-16 16:40:27 +0100

    dataRouter and reportController

--- api/controllers/reportController.js
+++ api/controllers/reportController.js
@@ -13,17 +13,40 @@ const getContry = async (req, res) => {
 
 const getDataWithYear = async (req, res) => {
     try {
-        const {id, year} = req.body;
-        const data = await Report.findOne({_id: id}).find( { "years": { $year: year } } )
-        res.status(200).json({sucess: true, data: data});
+        const { id, year } = req.body;
+        const data = await Report.findOne({ _id: id }).find({ "years": { $year: year } })
+        res.status(200).json({ sucess: true, data: data });
     } catch (error) {
         console.error(error);
-        res.status(500).json({error: error});
+        res.status(500).json({ error: error });
     }
 }
 
+const getDataWithGas = async (req, res) => {
+    try {
+        const { gas } = req.body;
+        const data = await Report.findOne({ gas: gas });
+        res.status(200).json({ sucess: true, data: data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error });
+    }
+}
+
+const getDataWithSector = async (req, res) => {
+    try {
+        const { sector } = req.body;
+        const data = await Report.findOne({ sector: sector });
+        res.status(200).json({ sucess: true, data: data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error });
+    }
+}
 
 export default {
     getContry,
     getDataWithYear,
-  }
+    getDataWithGas,
+    getDataWithSector
+}
