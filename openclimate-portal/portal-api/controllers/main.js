f46efd9c6ca8837d1dff452572701e7841a32302
David Thomson <davidthomson@Davids-MacBook-Pro-2.local>
2019-11-16 11:38:06 -0800

    Remove unused methods

--- openclimate-portal/portal-api/controllers/main.js
+++ openclimate-portal/portal-api/controllers/main.js
@@ -1,14 +1,4 @@
-const getTableData = (req, res, db) => {
-  db.select('*').from('testtable1')
-    .then(items => {
-      if(items.length){
-        res.json(items)
-      } else {
-        res.json({dataExists: 'false'})
-      }
-    })
-    .catch(err => res.status(400).json({dbError: 'db error'}))
-}
+
 
 
 const getEmissionsPledgesForCountry = (req, res, db) => {
@@ -80,10 +70,6 @@ function finishEmissionsPledgesForCountry  (req, res, db, country_code, passedRe
     from('cw_ndc_quantification_csv')
         .whereRaw('label != \'BAU\' AND iso = ?', [country_code])
         .then(items => {
-            console.log('Items: '+JSON.stringify(items));
-                //res.json(items[0])
-                //this.returnResult = items;
-                //console.log('Result so far: '+JSON.stringify(this.returnResult));
 
                let combined = [{}];
               combined[0].total_emissions = passedResult.total_emissions;
@@ -144,43 +130,9 @@ const getCities = (req, res, db) => {
         .catch(err => res.status(400).json({dbError: 'db error'}))
 }
 
-const postTableData = (req, res, db) => {
-  const { first, last, email, phone, location, hobby } = req.body
-  const added = new Date()
-  db('testtable1').insert({first, last, email, phone, location, hobby, added})
-    .returning('*')
-    .then(item => {
-      res.json(item)
-    })
-    .catch(err => res.status(400).json({dbError: 'db error'}))
-}
-
-const putTableData = (req, res, db) => {
-  const { id, first, last, email, phone, location, hobby } = req.body
-  db('testtable1').where({id}).update({first, last, email, phone, location, hobby})
-    .returning('*')
-    .then(item => {
-      res.json(item)
-    })
-    .catch(err => res.status(400).json({dbError: 'db error'}))
-}
-
-const deleteTableData = (req, res, db) => {
-  const { id } = req.body
-  db('testtable1').where({id}).del()
-    .then(() => {
-      res.json({delete: 'true'})
-    })
-    .catch(err => res.status(400).json({dbError: 'db error'}))
-}
-
 module.exports = {
     getCountries,
     getCities,
     getEmissions,
-    getEmissionsPledgesForCountry,
-    getTableData,
-    postTableData,
-    putTableData,
-    deleteTableData
+    getEmissionsPledgesForCountry
 }
