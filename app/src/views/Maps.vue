7f0118621ef5df22c324f9fcc981da75d1126863
owalid <othmane.el.ayadi@gmail.com>
2019-11-17 10:32:46 +0100

    add field

--- app/src/views/Maps.vue
+++ app/src/views/Maps.vue
@@ -4,41 +4,77 @@
       <v-row align="center" justify="center">
         <v-col cols="2">
           <svg-map :map="Australia" />
+          <v-row align="end" justify="center">
+            <p>Australia</p>
+          </v-row>
         </v-col>
         <v-col cols="2">
           <svg-map :map="Taiwan" />
+          <v-row align="end" justify="center">
+            <p>Taiwan</p>
+          </v-row>
         </v-col>
         <v-col cols="2">
           <svg-map :map="FranceRegions" />
+          <v-row align="end" justify="center">
+            <p>France</p>
+          </v-row>
         </v-col>
         <v-col cols="2">
           <svg-map :map="Canada" />
+          <v-row align="end" justify="center">
+            <p>Canada</p>
+          </v-row>
         </v-col>
         <v-col cols="2">
           <svg-map :map="Germany" />
+          <v-row align="end" justify="center">
+            <p>Germany</p>
+          </v-row>
         </v-col>
         <v-col cols="2">
           <svg-map :map="HongKong" />
+          <v-row align="end" justify="center">
+            <p>HongKong</p>
+          </v-row>
         </v-col>
       </v-row>
       <v-row align="center" justify="center">
         <v-col cols="2">
           <svg-map :map="India" />
+          <v-row align="end" justify="center">
+            <p>India</p>
+          </v-row>
         </v-col>
         <v-col cols="2">
           <svg-map :map="Japan" />
+          <v-row align="end" justify="center">
+            <p>Japan</p>
+          </v-row>
         </v-col>
         <v-col cols="2">
           <svg-map :map="NewZealand" />
+          <v-row align="end" justify="center">
+            <p>NewZealand</p>
+          </v-row>
         </v-col>
         <v-col cols="2">
           <svg-map :map="SouthKorea" />
+          <v-row align="end" justify="center">
+            <p>SouthKorea</p>
+          </v-row>
         </v-col>
         <v-col cols="2">
           <svg-map :map="Tunisia" />
+          <v-row align="end" justify="center">
+            <p>Tunisia</p>
+          </v-row>
         </v-col>
         <v-col cols="2">
           <svg-map :map="Usa" />
+          <v-row align="end" justify="center">
+            <p>Usa</p>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
