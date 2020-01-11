66be3567a9fb5a062b207fd6f3d4db56ed18470f
Steven Ettinger <sjettingerjr@gmail.com>
2020-01-10 18:59:22 -0800

    RPC

--- sawtooth_api/index.js
+++ sawtooth_api/index.js
@@ -5,9 +5,141 @@ const express = require('express');
 const app = express()
 const port = ENV.PORT || 2999;
 const cbor = require('cbor');
+const request = require('request');
+const crypto = require('crypto');
+
+const { createHash } = require('crypto');
+const { protobuf } = require('sawtooth-sdk');
+
+
+const { createContext, CryptoFactory } = require('sawtooth-sdk/signing');
+
+const context = createContext('secp256k1');
+const privateKey = context.newRandomPrivateKey(); //anonymous keys while in dev... need identity for a key management solution
+console.log(privateKey) // for recall? 
+
+// Supported Proxied Addresses
+const INT_KEY_FAMILY = 'intkey'
+const INT_KEY_NAMESPACE = _hash(INT_KEY_FAMILY).substring(0, 6)
+
+const EMISSIONS_KEY_FAMILY = 'emre'
+const EMISSIONS_KEY_NAMESPACE = _hash(EMISSIONS_KEY_FAMILY).substring(0, 6)
+
+
+
+
+// Express Server & Proxied Events & RPC
 
 app.use(cors())
+app.get('/adv-build', (req, res, next) => { //way to build payloads for processing, should be key controled in future 
+  let payload,
+    payloadBytes,
+    inputs = [],
+    outputs = []
+
+  switch (req.query.family) {
+    case 'emre':
+      payload = {
+        [req.query.vt || 'name']: req.query.verb || '0002019CO28642',
+        [req.query.nt || 'action']: req.query.action || 'create',
+        [req.query.at || 'desc']: req.query.desc || '0123'
+        /*cbor.encode(req.query.desc || {
+          gas_type: 'CO2', //standardize these
+          sector: 'TEST',
+          last_reported_year: '2019', //4 digit
+          last_reported_mmtco2e: '8642', //nearest mmton
+          country: '000', //3 digit ISO 
+          data_source: 'Test',
+          data_reporter_public_key: 'TestKey',
+          identity: 'QmTEST',
+        })
+        */
+      }
+      inputs.push(EMISSIONS_KEY_NAMESPACE + _hash(payload.name).slice(-64)) //test for allowed names?
+      outputs.push(EMISSIONS_KEY_NAMESPACE + _hash(payload.name).slice(-64))
+      payloadBytes = cbor.encode(payload)
+      break;
+    case 'intkey'://intkey
+    default: //intkey
+      var payload = {
+        [req.query.vt || 'Verb']: req.query.verb || 'inc',
+        [req.query.nt || 'Name']: req.query.action || 'tes',
+        [req.query.at || 'Value']: req.query.desc || parseInt(req.query.num) || 1
+      }
+      if (req.query.addr || req.query.rt) {
+        payload[req.query.rt || 'addr'] = req.query.addr || null
+      }
+      inputs.push(INT_KEY_NAMESPACE + _hash(payload.Name).slice(-64))
+      outputs.push(INT_KEY_NAMESPACE + _hash(payload.Name).slice(-64))
+      payloadBytes = cbor.encode(payload)
+  }
+
+  const transactionHeaderBytes = protobuf.TransactionHeader.encode({
+    familyName: req.query.family || 'intkey',
+    familyVersion: req.query.version || '1.0',
+    inputs: inputs,
+    outputs: outputs,
+    signerPublicKey: signer.getPublicKey().asHex(),
+    batcherPublicKey: signer.getPublicKey().asHex(),
+    dependencies: [],
+    payloadSha512: createHash('sha512').update(payloadBytes).digest('hex')
+  }).finish()
+  const signature = signer.sign(transactionHeaderBytes)
+
+  const transaction = protobuf.Transaction.create({
+    header: transactionHeaderBytes,
+    headerSignature: signature,
+    payload: payloadBytes
+  })
+  const transactions = [transaction]
+  const batchHeaderBytes = protobuf.BatchHeader.encode({
+    signerPublicKey: signer.getPublicKey().asHex(),
+    transactionIds: transactions.map((txn) => txn.headerSignature),
+  }).finish()
+
+  const bsignature = signer.sign(batchHeaderBytes)
+
+  const batch = protobuf.Batch.create({
+    header: batchHeaderBytes,
+    headerSignature: bsignature,
+    transactions: transactions
+  })
+
+  const batchListBytes = protobuf.BatchList.encode({
+    batches: [batch]
+  }).finish()
+  request.post({
+    url: 'http://127.0.0.1:8008/batches',
+    body: batchListBytes,
+    headers: { 'Content-Type': 'application/octet-stream' }
+  }, (err, response) => {
+    var link = JSON.parse(response.body).link
+    if (err) {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({ payload, err, res: response.body }, null, 3))
+      console.log(err)
+    } else {
+      if (link) {
+        console.log('Followed: ' + link)
+        request.get({
+          url: link
+        }, (e, r) => {
+          if (e) return console.log(e)
+          res.setHeader('Content-Type', 'application/json');
+          var body = JSON.parse(r.body)
+          var link = body.link.replace(/127.0.0.1:8008/g, req.headers.host)
+          res.send(JSON.stringify({ payload, data: body.data, link }, null, 3))
+        })
+      } else {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ payload, body: JSON.parse(response.body) }, null, 3))
+      }
+    }
+  })
+})
+
 app.get('/state', (req, res, next) => {
+  console.log('stateReq')
   //let address = re
   child_process.exec('sawtooth state list', (err, stdout, stderr) => {
     if (err) {
@@ -21,7 +153,6 @@ app.get('/state', (req, res, next) => {
       HEAD_BLOCK: data[data.length - 2].split('"')[1],
       addresses: []
     }
-    console.log(sizespot, dataspot)
     for (i = 1; i < (data.length - 2); i++) {
       j = {
         ADDRESS: data[i].split(' ')[0],
@@ -46,36 +177,36 @@ app.get('/state/:id', (req, res, next) => {
     }
 
     res.setHeader('Content-Type', 'application/json');
-    var obj = {}
-    var data = stdout.split('\n')[0]
-    var raw = data.split('"')[1]
-    obj[data.split(':')[0]] = {
-      raw
-    }
     switch (address.substr(0, 6)) { //decode different namespace per cponventions
+      /*
       case '1cf126': //intkey decode
-        //var buf = new Buffer.from(obj[data.split(':')[0]].raw.split("'"), 'base64')
-
-        //var str = buf.toString('ascii')
-        //console.log(str)
-        //var bufs = obj[data.split(':')[0]].raw.split("'")[1].split("\\x")
-        cbor.decodeFirst(bufs[1], (err, o) => {
-          if (err) {
-            console.log(err)
-          } else {
-            obj[data.split(':')[0]].decoded = o
-          }
-          console.log(o)
-          res.send(JSON.stringify(obj, null, 3))
-        })
+        console.log(stdout)
+        cbor.decodeFirst("b'\xa1cbar\x18\x8f'", (err, obj) => (err ? console.log(err) : console.log(obj)))
+        cbor.decodeFirst(JSON.stringify(stdout), (err, obj) => (err ? console.log(err) : res.send(JSON.stringify(obj), null, 3)))
         break;
+        */
       case '000000': //settings transaction family
 
         break;
       default:
-        res.send(JSON.stringify(obj, null, 3))
+        res.send(JSON.stringify(stdout, null, 3))
     }
   });
+});
+
+app.get('/batch_statuses', (req, res, next) => {
+  var id = req.query.id
+  request.get({
+    url: `http://127.0.0.1:8008/batch_statuses?id=${id}`
+  }, (e, r) => {
+    if (e) return console.log(e)
+    res.setHeader('Content-Type', 'application/json'); //sanitize this link
+    var body = JSON.parse(r.body)
+    var link = body.link.replace(/127.0.0.1:8008/g, req.headers.host)
+    console.log(link)
+    body.link = link
+    res.send(JSON.stringify(body, null, 3))
+  })
 });
 
 app.listen(port, () => console.log(`SHP API listening on port ${port}!`))
